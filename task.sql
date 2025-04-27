33d395771bd2e64837f86f3f28a54a58efa0edd0
Danylo Melnychuk <danyakid2005@gmail.com>
2025-04-27 17:10:18 +0300

    solution

--- task.sql
+++ task.sql
@@ -1,47 +1,6 @@
-CREATE DATABASE IF NOT EXISTS ShopDB;
 USE ShopDB;
 
-CREATE TABLE IF NOT EXISTS Products (
-    ID INT AUTO_INCREMENT,
-    Name VARCHAR(50),
-    Description VARCHAR(100),
-    Price INT,
-    WarehouseAmount INT,
-    PRIMARY KEY (ID)
-);
-
-CREATE TABLE IF NOT EXISTS Customers (
-    ID INT AUTO_INCREMENT,
-    FirstName VARCHAR(50),
-    LastName VARCHAR(50),
-    Email VARCHAR(50),
-    Address VARCHAR(50),
-    PRIMARY KEY (ID)
-);
-
-CREATE TABLE IF NOT EXISTS Orders (
-    ID INT AUTO_INCREMENT,
-    CustomerID INT, 
-    FOREIGN KEY (CustomerID) REFERENCES Customers(ID) ON DELETE SET NULL,
-    Date DATE,
-    PRIMARY KEY (ID)
-);
-
-CREATE TABLE IF NOT EXISTS OrderItems (
-    ID INT AUTO_INCREMENT,
-    OrderID INT, 
-    FOREIGN KEY (OrderID) REFERENCES Orders(ID) ON DELETE SET NULL,
-	ProductID INT, 
-    FOREIGN KEY (ProductID) REFERENCES Products(ID) ON DELETE SET NULL,
-    PRIMARY KEY (ID)
-);
-
-CREATE INDEX IF NOT EXISTS Email ON Customers(Email);
-
-CREATE INDEX IF NOT EXISTS Name ON Products(Name);
-
-CREATE INDEX IF NOT EXISTS CustomerID ON Orders(CustomerID);
-
-CREATE INDEX IF NOT EXISTS Date ON Orders(Date);
-
-CREATE INDEX IF NOT EXISTS Price ON Products(Price);
+CREATE UNIQUE INDEX Email ON Customers(Email);
+CREATE INDEX Name ON Products(Name);
+CREATE INDEX CustomerID ON Orders(CustomerID);
+CREATE INDEX Date ON Orders(Date);
