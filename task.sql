2f3e5ee8fbcac20a032e3ca4e2008f1f8509c11f
Danylo Melnychuk <danyakid2005@gmail.com>
2025-04-27 03:29:16 +0300

    optimize database with indexes

--- task.sql
+++ task.sql
@@ -1,2 +1,12 @@
 # Write your SQL code for the database creation here. Good luck! 
-USE ShopDB; 
+USE ShopDB;
+
+CREATE UNIQUE INDEX Email ON Customers(Email);
+
+CREATE INDEX Name ON Products(Name);
+
+CREATE INDEX CustomerID ON Orders(CustomerID);
+
+CREATE INDEX Date ON Orders(Date);
+
+CREATE INDEX Price ON Products(Price);
